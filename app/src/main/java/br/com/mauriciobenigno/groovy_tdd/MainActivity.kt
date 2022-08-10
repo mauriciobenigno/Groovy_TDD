e2d833f7aa0ab329cbae0b8a662b1406db6e28a1
Mauricio Benigno <mauricio.soares@maximasistemas.com.br>
2022-08-10 16:32:18 -0300

    fix: inflate fragment

--- app/src/main/java/br/com/mauriciobenigno/groovy_tdd/MainActivity.kt
+++ app/src/main/java/br/com/mauriciobenigno/groovy_tdd/MainActivity.kt
@@ -2,12 +2,19 @@ package br.com.mauriciobenigno.groovy_tdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.mauriciobenigno.groovy_tdd.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
